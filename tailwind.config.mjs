b9ce312458337e8650fbec94af7c09793f17c6ac
Kerolous Amged <kerolous.amged@nagwa.com>
2024-03-15 19:27:21 +0200

    feat:added layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode : "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': 'rgb(14 19 25)',
+				'secondary': 'rgb(16 23 31)',
+			  },
+			gridTemplateRows:{
+				'layout': '60px 1fr',
+			},
+			gridTemplateColumns:{
+				'layout': '250px 1fr',
+			},
+			
+		},
 	},
 	plugins: [],
 }
