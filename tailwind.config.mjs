7a0f33769beac63cf81c18e121fa65bcaa03fef5
Kerolous Amged <kerolous.amged@nagwa.com>
2024-03-15 22:27:09 +0200

    feat:added background color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,10 @@ export default {
 			colors: {
 				'primary': 'rgb(14 19 25)',
 				'secondary': 'rgb(16 23 31)',
-				text : 'rgb(102 110 119)'
+				text : 'rgb(102 110 119)',
+				'radial-centerd' : "radial-gradient(circle at center, #1D2B3B, #0F161E)"
 			  },
+			
 			gridTemplateRows:{
 				'layout': '45px 1fr',
 			},
@@ -20,3 +22,6 @@ export default {
 	},
 	plugins: [],
 }
+
+
+// background: radial-gradient(at right bottom, #1D2B3B, #0F161E);
