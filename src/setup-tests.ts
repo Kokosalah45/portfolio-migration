917c81b81deefbb74f8046ebc1602fd7e129dc4f
Kerolous Amged <kerolous.amged@nagwa.com>
2024-03-16 09:38:13 +0200

    feat:design colors and some cleanup

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -1,11 +1,3 @@
 import "@testing-library/svelte/vitest";
 import "@testing-library/jest-dom/vitest";
-import { vi } from "vitest";
 
-
-Object.assign(window.navigator, {
-    clipboard: {
-      writeText: vi.fn().mockImplementation(() => Promise.resolve()),
-      
-    },
-  });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,16 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'primary': 'rgb(14 19 25)',
-				'secondary': 'rgb(16 23 31)',
-				text : 'rgb(102 110 119)',
-				'radial-centerd' : "radial-gradient(circle at center, #1D2B3B, #0F161E)"
+				primary: 'rgb(14 19 25)',
+				secondary: 'rgb(16 23 31)',
+				title : '#FFF',
+				subtitle : '#7a9fbb',
+				separator : '#272f3c',
+				text : '#848d94',
+				'secondary-text' : '#333d48',
+				'active-text' : '#b5a07a',
+				'sperator-hightlight' : '#8d4551',
+
 			  },
 			
 			gridTemplateRows:{
